4e5722deaf0b6152bfa1e68baa85b259d4de153d
Marcus Escobar <marcusescobar10@gmail.com>
2023-11-01 10:25:19 -0300

    Scanner Main

--- trab_3Etapa/src/main/Main.java
+++ trab_3Etapa/src/main/Main.java
@@ -1,7 +1,11 @@
 package main;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);//Entrada 
+        
+
         
     }
 }
